f57342192ca6c4502e025caf758a7ef3b346a60e
Elviex123 <125544065+Elviex123@users.noreply.github.com>
2023-03-20 16:10:52 +0800

    Update ConsoleApplication8.cpp

--- ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -2,37 +2,40 @@
 //
 
 #include<iostream>  
-using namespace std;
+#include<string>  
+#include<sstream>   
+using namespace std;  
 
-int main()
-{
-    int input = 0;
-
-    cin >> input;
-    while (input)
-    {
-        int a = 0, ai = 0, b = 0, bi = 0;
-        char ch;
-
-        cin >> ch >> ai >> a >> bi >> b;
-        switch (ch)   
-        { 
-            case '+':
-                cout << ai + bi << " " << a + b << endl; 
-                break;
-            case '-':
-                cout << ai - bi << " " << a - b << endl; 
-                break;
-            case '*'
-                :cout << ai * bi - a * b << " " << a * bi + ai * b << endl; 
-                break;
-            case '/'
-                :cout << (ai * bi + a * b) / (bi * bi + b * b) << " " << a * bi - ai * b / (bi * bi + b * b) << endl;
-                break;
-        }
-    }
-    return 0;
-}
+int main()  
+{  
+    int inputBus=0,inputLine=0;  
+  
+    cin>>inputBus>>inputLine;
+    cin.ignore(1,'\n'); 
+    int reArray[inputLine][inputBus];  
+    for(int bus=0;bus<inputBus;bus++)
+    {  
+        string nums;  
+        getline(cin,nums);  
+        istringstream cutStr(nums);  
+        for(int line=0;line<inputLine;line++)
+        {
+            string save;  
+            getline(cutStr,save,' ');  
+            istringstream chType(save);  
+            chType>>reArray[line][bus];
+        }  
+    }  
+    for(int i=0;i<inputLine;i++)
+    {  
+        for(int j=0;j<inputBus;j++)  
+        {  
+            if(j==inputBus-1) cout<<reArray[i][j]<<endl;  
+            else cout<<reArray[i][j]<<" ";  
+        }  
+    }  
+    return 0;  
+}  
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
